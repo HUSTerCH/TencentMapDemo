d665d76b873c536024829f6407bffb013569dae8
HUSTerCH <93471031+HUSTerCH@users.noreply.github.com>
2022-08-05 16:54:38 +0800

    stash

--- app/src/main/java/com/fengsheng/tencentmapdemo/MainActivity.kt
+++ app/src/main/java/com/fengsheng/tencentmapdemo/MainActivity.kt
@@ -1,31 +1,37 @@
 package com.fengsheng.tencentmapdemo
 
-import android.graphics.SurfaceTexture
+import android.app.Activity
 import android.os.Bundle
-import android.view.View
-import android.widget.RelativeLayout
-import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
-import com.tencent.tencentmap.mapsdk.maps.MapRenderLayer
-import com.tencent.tencentmap.mapsdk.maps.TencentMap
-import com.tencent.tencentmap.mapsdk.maps.TencentMapOptions
+import com.amap.api.maps.MapView
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
+    private var mMapView: MapView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val view = findViewById<RelativeLayout>(R.id.mainLayout)
-        val surface = SurfaceTexture(100)
-        val mapOptions = TencentMapOptions()
-        mapOptions.extSurface = surface
-        mapOptions.setExtSurfaceDimension(900, 900)
-        val mapView = MapRenderLayer(this, mapOptions)
-        view.addView(mapView)
-        mapView.startLayoutAnimation()
-        val mMap : TencentMap = mapView.map
-        mMap.setOnMapLoadedCallback {
-            println("????")
-        }
+        mMapView = findViewById(R.id.map)
+        // 此方法须覆写，虚拟机需要在很多情况下保存地图绘制的当前状态。
+//        mapView.startLayoutAnimation()
+
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
+        mMapView?.onDestroy()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
+        mMapView?.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
+        mMapView?.onPause()
+    }
+
 }
